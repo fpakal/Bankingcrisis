c91a783d93f5511e7cb9fc4df28c88cf0b53f4d8
pwilczewski <pwilczewski@users.noreply.github.com>
2023-05-06 12:09:47 -0500

    data update

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import numpy as np
 from data_proc import load_equity_data, load_yield_curve_data, load_credit_spread_data, load_fed_balance_sheet
 from data_proc import plot_treasury_curves, plot_credit_spreads, plot_fed_balance_sheet
 
-loser_frame, largest_frame = load_equity_data()
-yc_frame = load_yield_curve_data()
-spread_frame = load_credit_spread_data()
-fed_frame = load_fed_balance_sheet()
+data_from_file = True
+
+loser_frame, largest_frame = load_equity_data(data_from_file)
+yc_frame = load_yield_curve_data(data_from_file)
+spread_frame = load_credit_spread_data(data_from_file)
+fed_frame = load_fed_balance_sheet(data_from_file)
 
 with gr.Blocks() as crisis_dashboard:
     gr.Markdown("<font size=36><center>Banking Crisis Dashboard</center></font>")
